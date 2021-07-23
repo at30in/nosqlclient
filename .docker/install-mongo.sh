3c70cce2b8617d2a27291d1290cc3ecec9792c12
Andrea Trentin <at30in@gmail.com>
2021-07-23 08:40:26 +0200

    Update mongo from 4.2 to 4.4

--- .docker/install-mongo.sh
+++ .docker/install-mongo.sh
@@ -3,16 +3,16 @@
 set -e
 
 if [ "$INSTALL_MONGO" = true ]; then
-  printf "\n[-] Installing MongoDB 4.2...\n\n"
+  printf "\n[-] Installing MongoDB 4.4...\n\n"
  
   cd /tmp
-  curl -O -L https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian92-v4.2-latest.tgz
-  mkdir mongodb-linux-x86_64-debian92-v4.2
-  tar xvzf mongodb-linux-x86_64-debian92-v4.2-latest.tgz --strip 1 -C mongodb-linux-x86_64-debian92-v4.2
-  rm mongodb-linux-x86_64-debian92-v4.2-latest.tgz
+  curl -O -L https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian92-v4.4-latest.tgz
+  mkdir mongodb-linux-x86_64-debian92-v4.4
+  tar xvzf mongodb-linux-x86_64-debian92-v4.4-latest.tgz --strip 1 -C mongodb-linux-x86_64-debian92-v4.4
+  rm mongodb-linux-x86_64-debian92-v4.4-latest.tgz
 
   rm -rf /opt/mongodb
-  mv mongodb-linux-x86_64-debian92-v4.2 /opt/mongodb
+  mv mongodb-linux-x86_64-debian92-v4.4 /opt/mongodb
 
   ln -sf /opt/mongodb/bin/mongo /usr/bin/mongo
   ln -sf /opt/mongodb/bin/mongod /usr/bin/mongod
